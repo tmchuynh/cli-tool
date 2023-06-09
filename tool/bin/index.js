1cbfe2d237969ad804f957cd51d7dc7dfce6f450
tmchuynh <tinamchuynh@gmail.com>
2023-06-09 11:20:05 -0700

    update tool/bin/index.js and package.json

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const arg = require('arg');
+const chalk = require('chalk');
 
 try {
   const args = arg({
@@ -8,16 +9,16 @@ try {
   });
 
   if (args['--start']) {
-    console.log('starting the app');
+    console.log(chalk.bgCyanBright('starting the app'));
   }
 } catch (e) {
-  console.log(e.message);
+  console.log(chalk.yellow(e.message));
   console.log();
   usage();
 }
 
 function usage() {
-  console.log(`tool [CMD]
-  --start\tStarts the app
-  --build\tBuilds the app`);
+  console.log(`${chalk.whiteBright('tool [CMD]')}
+  ${chalk.greenBright('--start')}\tStarts the app
+  ${chalk.greenBright('--build')}\tBuilds the app`);
 }
