019296882663c25939f28c25b38a91a7433bf628
tmchuynh <tinamchuynh@gmail.com>
2023-06-09 11:26:07 -0700

    update config-mgr.js

--- tool/src/commands/config-mgr.js
+++ tool/src/commands/config-mgr.js
@@ -2,12 +2,14 @@ const chalk = require('chalk');
 const pkgUp = require('pkg-up');
 
 module.exports = function getConfig() {
-  const pkgPath = pkgUp.sync({cwd: process.cwd()});
-  const pkg = require(pkgPath);
-  if (pkg.tool) {
-    console.log('Found configuration', pkg.tool);
-    return pkg.tool;
-  } else {
-    console.log(chalk.yellow('Could not find configuration, using default'));
-    return { port: 1234 };
-  }
+    const pkgPath = pkgUp.sync({ cwd: process.cwd() });
+    const pkg = require(pkgPath);
+    if (pkg.tool) {
+        console.log('Found configuration', pkg.tool);
+        return pkg.tool;
+    } else if (hasJSConfigFile()) { // <- pseudo code
+        return loadJSConfigFile(); // <- pseudo code
+    } else {
+        console.log(chalk.yellow('Could not find configuration, using default'));
+        return { port: 1234 };
+    }
